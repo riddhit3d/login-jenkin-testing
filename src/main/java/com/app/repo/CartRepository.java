38589e50bd3ee6bdb0a9d7eb60e75b5fde7b5d7b
riddhi <riddhit@cybage.com>
2024-01-29 18:50:31 +0530

    first commit

---
+++ src/main/java/com/app/repo/CartRepository.java
@@ -0,0 +1,35 @@
+//package com.app.repo;
+//
+//
+//
+//import java.beans.JavaBean;
+//import java.util.List;
+//import java.util.Optional;
+//
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.stereotype.Component;
+//import org.springframework.stereotype.Repository;
+//
+//import com.app.entity.User;
+//import com.app.entity.Cart;
+//import com.app.entity.Cart1;
+//import com.app.entity.Product;
+//@JavaBean
+//@Component
+//@Repository
+//public interface CartRepository extends JpaRepository< Cart,Integer>{
+//
+//
+//public Object findByUserAndProduct(User user, Product product);
+//
+//public List<Cart> findByUser(User user);
+//
+//public void save(Cart1 wl);
+//
+//
+//
+//
+//
+//
+//	
+//}
